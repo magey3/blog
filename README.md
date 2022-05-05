903b485a8aa6776b0c144355f205bd4754313c02
Jozef <jabczunjozef@gmail.com>
2022-05-05 19:06:20 +0200

    lawyer stuff

--- README.md
+++ README.md
@@ -32,3 +32,8 @@ You can check out [the Next.js GitHub repository](https://github.com/vercel/next
 The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
 
 Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+
+## License
+This project with the exception of all files in the folder [pages/posts](pages/posts/) is licensed under the [MIT license](LICENSE-MIT).
+All files in the folder [pages/posts](pages/posts/) are licensed under the
+[Creative Commons Attribution-NonCommercial 4.0 International Public License](LICENSE-CC-BY-NC-4.0).
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import Stack from 'react-bootstrap/Stack';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
+import Navbar from 'react-bootstrap/Navbar';
 
 import _Navbar from '../components/navbar'
 import PostEntry from '../components/postentry'
@@ -19,14 +20,25 @@ const Home: NextPage = () => {
 	    </Head>
 	    <_Navbar />
 	    <Container className="mt-3">
-		<Row>
-		<Col/>
-		<Col sm={7}>
-		    <Stack gap="{3}">
-			<PostEntry title="Making this site" date="2022-5-5" href="/posts/makingthissite" />
-		    </Stack>
-		</Col>
-		<Col/>
+		<Row auto>
+		    <Col/>
+		    <Col sm={7}>
+			<Stack gap="{3}">
+			    <PostEntry title="Making this site" date="2022-5-5" href="/posts/makingthissite" />
+			</Stack>
+
+		    </Col>
+		    <Col/>
+		</Row>
+		<Row/>
+		<Row className="fixed-bottom">
+		    <Col/>
+		    <Col sm={7}>
+			Copyright (C) 2022 Jozef Jabczun
+			Site code is licensed under the MIT license. Posts are licensed under the CC-BY-NC-4.0 license.
+			See README.md on the <a href="https://github.com/magey3/blog">github</a> for details.
+		    </Col>
+		    <Col/>
 		</Row>
 	    </Container>
 	</div>
